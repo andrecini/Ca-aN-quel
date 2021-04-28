4d2c0d90c21436f029d14a1b39495e8eb305fa20
andrecini <andrevpcini@gmail.com>
2021-04-28 18:48:37 -0300

    Exemplo

---
+++ Form1.Designer.cs
@@ -0,0 +1,379 @@
+﻿namespace ProjetoCacaNiquel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Roleta = new System.Windows.Forms.Panel();
+            this.picA3 = new System.Windows.Forms.PictureBox();
+            this.picC5 = new System.Windows.Forms.PictureBox();
+            this.picB3 = new System.Windows.Forms.PictureBox();
+            this.picA1 = new System.Windows.Forms.PictureBox();
+            this.picA5 = new System.Windows.Forms.PictureBox();
+            this.picB1 = new System.Windows.Forms.PictureBox();
+            this.picB5 = new System.Windows.Forms.PictureBox();
+            this.picC3 = new System.Windows.Forms.PictureBox();
+            this.picC4 = new System.Windows.Forms.PictureBox();
+            this.picA4 = new System.Windows.Forms.PictureBox();
+            this.picC2 = new System.Windows.Forms.PictureBox();
+            this.picB4 = new System.Windows.Forms.PictureBox();
+            this.picC1 = new System.Windows.Forms.PictureBox();
+            this.picA2 = new System.Windows.Forms.PictureBox();
+            this.picB2 = new System.Windows.Forms.PictureBox();
+            this.panel3.SuspendLayout();
+            this.Roleta.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picA3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picA1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picA5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picA4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picA2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Location = new System.Drawing.Point(98, 302);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(100, 93);
+            this.panel3.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(216)))), ((int)(((byte)(86)))));
+            this.button1.Font = new System.Drawing.Font("Stencil", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 86);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ON";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "cereja.png");
+            this.imageList.Images.SetKeyName(1, "gold.png");
+            this.imageList.Images.SetKeyName(2, "Seven.png");
+            this.imageList.Images.SetKeyName(3, "laranja.png");
+            this.imageList.Images.SetKeyName(4, "trevo.png");
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Roleta
+            // 
+            this.Roleta.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Roleta.Controls.Add(this.picA3);
+            this.Roleta.Controls.Add(this.picC5);
+            this.Roleta.Controls.Add(this.picB3);
+            this.Roleta.Controls.Add(this.picA1);
+            this.Roleta.Controls.Add(this.picA5);
+            this.Roleta.Controls.Add(this.picB1);
+            this.Roleta.Controls.Add(this.picB5);
+            this.Roleta.Controls.Add(this.picC3);
+            this.Roleta.Controls.Add(this.picC4);
+            this.Roleta.Controls.Add(this.picA4);
+            this.Roleta.Controls.Add(this.picC2);
+            this.Roleta.Controls.Add(this.picB4);
+            this.Roleta.Controls.Add(this.picC1);
+            this.Roleta.Controls.Add(this.picA2);
+            this.Roleta.Controls.Add(this.picB2);
+            this.Roleta.Location = new System.Drawing.Point(0, 0);
+            this.Roleta.Name = "Roleta";
+            this.Roleta.Size = new System.Drawing.Size(300, 300);
+            this.Roleta.TabIndex = 25;
+            // 
+            // picA3
+            // 
+            this.picA3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picA3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picA3.Location = new System.Drawing.Point(0, 200);
+            this.picA3.Margin = new System.Windows.Forms.Padding(0);
+            this.picA3.Name = "picA3";
+            this.picA3.Size = new System.Drawing.Size(100, 100);
+            this.picA3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picA3.TabIndex = 16;
+            this.picA3.TabStop = false;
+            this.picA3.Tag = "A";
+            // 
+            // picC5
+            // 
+            this.picC5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picC5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picC5.Location = new System.Drawing.Point(199, 400);
+            this.picC5.Margin = new System.Windows.Forms.Padding(0);
+            this.picC5.Name = "picC5";
+            this.picC5.Size = new System.Drawing.Size(100, 100);
+            this.picC5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picC5.TabIndex = 24;
+            this.picC5.TabStop = false;
+            this.picC5.Tag = "C";
+            // 
+            // picB3
+            // 
+            this.picB3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picB3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picB3.Location = new System.Drawing.Point(99, 200);
+            this.picB3.Margin = new System.Windows.Forms.Padding(0);
+            this.picB3.Name = "picB3";
+            this.picB3.Size = new System.Drawing.Size(100, 100);
+            this.picB3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picB3.TabIndex = 17;
+            this.picB3.TabStop = false;
+            this.picB3.Tag = "B";
+            // 
+            // picA1
+            // 
+            this.picA1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picA1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picA1.Location = new System.Drawing.Point(0, 0);
+            this.picA1.Margin = new System.Windows.Forms.Padding(0);
+            this.picA1.Name = "picA1";
+            this.picA1.Size = new System.Drawing.Size(100, 100);
+            this.picA1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picA1.TabIndex = 14;
+            this.picA1.TabStop = false;
+            this.picA1.Tag = "A";
+            // 
+            // picA5
+            // 
+            this.picA5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picA5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picA5.Location = new System.Drawing.Point(0, 400);
+            this.picA5.Margin = new System.Windows.Forms.Padding(0);
+            this.picA5.Name = "picA5";
+            this.picA5.Size = new System.Drawing.Size(100, 100);
+            this.picA5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picA5.TabIndex = 22;
+            this.picA5.TabStop = false;
+            this.picA5.Tag = "A";
+            // 
+            // picB1
+            // 
+            this.picB1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picB1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picB1.Location = new System.Drawing.Point(100, 0);
+            this.picB1.Margin = new System.Windows.Forms.Padding(0);
+            this.picB1.Name = "picB1";
+            this.picB1.Size = new System.Drawing.Size(100, 100);
+            this.picB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picB1.TabIndex = 13;
+            this.picB1.TabStop = false;
+            this.picB1.Tag = "B";
+            // 
+            // picB5
+            // 
+            this.picB5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picB5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picB5.Location = new System.Drawing.Point(99, 400);
+            this.picB5.Margin = new System.Windows.Forms.Padding(0);
+            this.picB5.Name = "picB5";
+            this.picB5.Size = new System.Drawing.Size(100, 100);
+            this.picB5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picB5.TabIndex = 23;
+            this.picB5.TabStop = false;
+            this.picB5.Tag = "B";
+            // 
+            // picC3
+            // 
+            this.picC3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picC3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picC3.Location = new System.Drawing.Point(199, 200);
+            this.picC3.Margin = new System.Windows.Forms.Padding(0);
+            this.picC3.Name = "picC3";
+            this.picC3.Size = new System.Drawing.Size(100, 100);
+            this.picC3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picC3.TabIndex = 18;
+            this.picC3.TabStop = false;
+            this.picC3.Tag = "C";
+            // 
+            // picC4
+            // 
+            this.picC4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picC4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picC4.Location = new System.Drawing.Point(199, 300);
+            this.picC4.Margin = new System.Windows.Forms.Padding(0);
+            this.picC4.Name = "picC4";
+            this.picC4.Size = new System.Drawing.Size(100, 100);
+            this.picC4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picC4.TabIndex = 21;
+            this.picC4.TabStop = false;
+            this.picC4.Tag = "C";
+            // 
+            // picA4
+            // 
+            this.picA4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picA4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picA4.Location = new System.Drawing.Point(0, 300);
+            this.picA4.Margin = new System.Windows.Forms.Padding(0);
+            this.picA4.Name = "picA4";
+            this.picA4.Size = new System.Drawing.Size(100, 100);
+            this.picA4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picA4.TabIndex = 19;
+            this.picA4.TabStop = false;
+            this.picA4.Tag = "A";
+            // 
+            // picC2
+            // 
+            this.picC2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picC2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picC2.Location = new System.Drawing.Point(199, 100);
+            this.picC2.Margin = new System.Windows.Forms.Padding(0);
+            this.picC2.Name = "picC2";
+            this.picC2.Size = new System.Drawing.Size(100, 100);
+            this.picC2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picC2.TabIndex = 12;
+            this.picC2.TabStop = false;
+            this.picC2.Tag = "C";
+            // 
+            // picB4
+            // 
+            this.picB4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picB4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picB4.Location = new System.Drawing.Point(99, 300);
+            this.picB4.Margin = new System.Windows.Forms.Padding(0);
+            this.picB4.Name = "picB4";
+            this.picB4.Size = new System.Drawing.Size(100, 100);
+            this.picB4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picB4.TabIndex = 20;
+            this.picB4.TabStop = false;
+            this.picB4.Tag = "B";
+            // 
+            // picC1
+            // 
+            this.picC1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picC1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picC1.Location = new System.Drawing.Point(200, 0);
+            this.picC1.Margin = new System.Windows.Forms.Padding(0);
+            this.picC1.Name = "picC1";
+            this.picC1.Size = new System.Drawing.Size(100, 100);
+            this.picC1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picC1.TabIndex = 15;
+            this.picC1.TabStop = false;
+            this.picC1.Tag = "C";
+            // 
+            // picA2
+            // 
+            this.picA2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picA2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picA2.Location = new System.Drawing.Point(0, 100);
+            this.picA2.Margin = new System.Windows.Forms.Padding(0);
+            this.picA2.Name = "picA2";
+            this.picA2.Size = new System.Drawing.Size(100, 100);
+            this.picA2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picA2.TabIndex = 10;
+            this.picA2.TabStop = false;
+            this.picA2.Tag = "A";
+            // 
+            // picB2
+            // 
+            this.picB2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picB2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picB2.Location = new System.Drawing.Point(99, 100);
+            this.picB2.Margin = new System.Windows.Forms.Padding(0);
+            this.picB2.Name = "picB2";
+            this.picB2.Size = new System.Drawing.Size(100, 100);
+            this.picB2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picB2.TabIndex = 11;
+            this.picB2.TabStop = false;
+            this.picB2.Tag = "B";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(303, 396);
+            this.Controls.Add(this.Roleta);
+            this.Controls.Add(this.panel3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel3.ResumeLayout(false);
+            this.Roleta.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picA3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picA1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picA5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picA4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picA2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel Roleta;
+        private System.Windows.Forms.PictureBox picA3;
+        private System.Windows.Forms.PictureBox picC5;
+        private System.Windows.Forms.PictureBox picB3;
+        private System.Windows.Forms.PictureBox picA1;
+        private System.Windows.Forms.PictureBox picA5;
+        private System.Windows.Forms.PictureBox picB1;
+        private System.Windows.Forms.PictureBox picB5;
+        private System.Windows.Forms.PictureBox picC3;
+        private System.Windows.Forms.PictureBox picC4;
+        private System.Windows.Forms.PictureBox picA4;
+        private System.Windows.Forms.PictureBox picC2;
+        private System.Windows.Forms.PictureBox picB4;
+        private System.Windows.Forms.PictureBox picC1;
+        private System.Windows.Forms.PictureBox picA2;
+        private System.Windows.Forms.PictureBox picB2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Threading;
+
+namespace ProjetoCacaNiquel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //ClearPictureColumn('A');
+            //ClearPictureColumn('B');
+            //ClearPictureColumn('C');
+        }
+
+        private static Random random = new Random();
+        private bool ativaMovimentoA = false;
+        private bool ativaMovimentoB = false;
+        private bool ativaMovimentoC = false;
+        private static int contadorTempo = 1;
+        private static int deslocamento = 25;
+        private static int multiplicadorVoltas;
+        private static int voltaCompleta = 4;
+        private static char[] columns = { 'A', 'B', 'C' };
+        private static int indexColumns = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetPictureColumn(columns[indexColumns]);
+            AtivaTimer(columns[indexColumns], timer1);
+
+            if (indexColumns == 2)
+            {
+                indexColumns = 0;
+            }
+            else
+            {
+                indexColumns++;
+            }
+
+        }
+
+        #region Controle Movimento das PictureBox's
+
+        public void Movimenta(bool ativaMovimento, PictureBox pic1, PictureBox pic2, PictureBox pic3, PictureBox pic4, PictureBox pic5)
+        {
+            if (ativaMovimento)
+            {
+                pic1.Top -= deslocamento;
+                pic2.Top -= deslocamento;
+                pic3.Top -= deslocamento;
+                pic4.Top -= deslocamento;
+                pic5.Top -= deslocamento;
+            }
+        }
+
+        public void VerificaExtremos(bool ativaMovimento, PictureBox pic1, PictureBox pic2, PictureBox pic3, PictureBox pic4, PictureBox pic5)
+        {
+            if (ativaMovimento)
+            {
+                if (pic1.Top <= -200)
+                {
+                    pic1.Top = 300;
+                }
+                if (pic2.Top <= -200)
+                {
+                    pic2.Top = 300;
+                }
+                if (pic3.Top <= -200)
+                {
+                    pic3.Top = 300;
+                }
+                if (pic4.Top <= -200)
+                {
+                    pic4.Top = 300;
+                }
+                if (pic5.Top <= -200)
+                {
+                    pic5.Top = 300;
+                }
+            }
+        }
+        
+        #endregion
+
+        #region Timers
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           if (contadorTempo <= voltaCompleta * multiplicadorVoltas)
+            {
+                if(ativaMovimentoA)
+                {
+                    Movimenta(ativaMovimentoA, picA1, picA2, picA3, picA4, picA5);
+                    VerificaExtremos(ativaMovimentoA, picA1, picA2, picA3, picA4, picA5);
+                }
+                else if(ativaMovimentoB)
+                {
+                    Movimenta(ativaMovimentoB, picB1, picB2, picB3, picB4, picB5);
+                    VerificaExtremos(ativaMovimentoB, picB1, picB2, picB3, picB4, picB5);
+                }
+                else if(ativaMovimentoC)
+                {
+                    Movimenta(ativaMovimentoC, picC1, picC2, picC3, picC4, picC5);
+                    VerificaExtremos(ativaMovimentoC, picC1, picC2, picC3, picC4, picC5);
+                }
+                
+                contadorTempo++;
+            }
+           else
+            {
+                timer1.Enabled = false;
+                ativaMovimentoA = false;
+                ativaMovimentoB = false;
+                ativaMovimentoC = false;
+                contadorTempo = 1;
+           }
+        }
+
+        private void AtivaTimer(char c, System.Windows.Forms.Timer t)
+        {
+            if (c == 'A')
+                ativaMovimentoA = true;
+            else if (c == 'B')
+                ativaMovimentoB = true;
+            else if (c == 'C')
+                ativaMovimentoC = true;
+
+            multiplicadorVoltas = random.Next(5, 15);
+            t.Enabled = true;
+        }
+
+        #endregion
+
+    }
+}
